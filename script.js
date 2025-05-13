47ca0f0a842e27141c12929d61cd5089ccfdc47c
kotkolil <ldxi1991@gmail.com>
2025-05-14 01:01:58 +0300

    added slider to set speed of enemy

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const stringArr1 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cra
 let stringArr = stringArr1
     .map(value => ({ value, sort: Math.random() }))
     .sort((a, b) => a.sort - b.sort)
-    .map(({ value }) => value);
+    .map(({ value }) => value).sort()
 
 var currentWord = 0;
 
@@ -13,48 +13,48 @@ function getRandomArbitrary(min, max) {
 
 
 $(document).ready(() => {
-    $("#start_button").on("click", () => {
-        $("#currentWord").text(stringArr[currentWord]);
+        $("#start_button").on("click", () => {
+            $("#currentWord").text(stringArr[currentWord]);
 
-        var carArr = [$("#car1"), $("#car2")];
+            var carArr = [$("#car1"), $("#car2")];
 
 
-var mainGame = setInterval(() => {
-    carArr.forEach((elem) => {
-        var currentPosition = parseInt(elem.css("margin-left"));
+            var mainGame = setInterval(() => {
+                carArr.forEach((elem) => {
+                    var currentPosition = parseInt(elem.css("margin-left"));
 
-        if (currentPosition > window.innerWidth * 0.8) {
-            alert("BOTS WINS! YOU ARE LOSER!");
-            console.log("Stopping game..."); // Debug log
-            clearInterval(mainGame); // Should stop here
-            return; // Exit the loop early
-        }
+                    if (currentPosition > window.innerWidth * 0.8) {
+                        alert("BOTS WINS! YOU ARE LOSER!");
+                        clearInterval(mainGame);
+                        return;
+                    }
+                    console.log($("#speedSlider").val())
+                    elem.css({"margin-left": (currentPosition + parseInt($("#speedSlider").val())) + "px"});
+                });
+            }, 50);
 
-        elem.css({ "margin-left": (currentPosition + 1) + "px" });
-    });
-}, 50);
-
-        $(document).on('keypress', (e) => {
-            playerCar = $("#car3")
-            var playerCurrentPosition = parseInt(playerCar.css("margin-left"));
-            if (e.key === "Enter") {
-                let answer = $("#answer").val();
-                if (answer === stringArr[currentWord]) {
-                    playerCar.css({ "margin-left": (playerCurrentPosition + 45) + "px" });
-                    $("#answer").val('');
-                    currentWord += 1;
-                    $("#currentWord").text(stringArr[currentWord]);
+            $(document).on('keypress', (e) => {
+                playerCar = $("#car3")
+                var playerCurrentPosition = parseInt(playerCar.css("margin-left"));
+                if (e.key === "Enter") {
+                    let answer = $("#answer").val();
+                    if (answer === stringArr[currentWord]) {
+                        playerCar.css({"margin-left": (playerCurrentPosition + 45) + "px"});
+                        $("#answer").val('');
+                        currentWord += 1;
+                        $("#currentWord").text(stringArr[currentWord]);
+                    }
+                    if (playerCurrentPosition > window.innerWidth * 0.76) {
+                        alert("YOU WIN!")
+                        clearInterval(mainGame);
+                    }
                 }
-                if (playerCurrentPosition > window.innerWidth * 0.76) {
-                    alert("YOU WIN!")
-                    clearInterval(mainGame);
-                }
-            }
+            });
+
+            $(document).on("paste", (e) => {
+                alert("Pasting is not allowed!");
+                e.preventDefault();
+            });
         });
-
-        // $(document).on("paste", (e) => {
-        //     alert("Pasting is not allowed!");
-        //     e.preventDefault();
-        // });
-    });
-});
+    }
+)
